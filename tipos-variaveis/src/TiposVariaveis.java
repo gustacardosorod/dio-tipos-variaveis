92c9d7956c5c60bcd6dade81815b304a1aacd1d6
gustavocardosorod <gustavocardoso1705@gmail.com>
2024-05-30 15:38:43 -0300

    Exemplos

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,9 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        short numeroCurto = 1;
+        short numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+    }
+
+}
